f1d4913e9a8142ab66a4dfd9647d387340d126e0
mohammad6987 <119624288+mohammad6987@users.noreply.github.com>
2022-12-27 16:06:45 +0330

    Create procces.php

---
+++ procces.php
@@ -0,0 +1,9 @@
+<?php
+ $myfile = fopen("testfile.txt", "w") 
+ fwrite($myfile, $esm);
+ fwrite($myfile, $esm2);
+ fwrite($myfile, $email);
+ fwrite($myfile, $massage);
+bool rename( string $testfile, string $text1 )
+
+?>
